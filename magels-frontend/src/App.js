7c8c84d976bd2202eb201afa515f71f8d2e42255
GeeksforGeeks <rlmagtira@gmail.com>
2025-06-23 16:22:05 +0800

    // FrontEnd Progress
    // established front end interface - approx 60 %

---
+++ magels-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Nav/Header';
+import Homepage from './component/Homepage';
+import Footer from './Nav/Footer';
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Homepage/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ magels-frontend/src/Controllers/ResetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div>
+      This is the change password
+    </div>
+  )
+}
+
+export default ResetPassword
